9d3bbc705eb985bb77bb39dc7415a18b961628f7
Rachael <94970084+Lovethrech@users.noreply.github.com>
2024-05-18 04:37:10 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'bells.exam.scheduling.system',
+  appName: 'bells-exam-scheduling-system',
+  webDir: 'dist'
+};
+
+export default config;
